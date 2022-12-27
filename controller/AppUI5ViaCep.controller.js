521af2bce15d053a4d7fbe5936ca577eb52f0635
Ney Brito <ney.brito@etec.sp.gov.br>
2022-12-27 23:58:24 +0000

    Correção de Bugs

--- controller/AppUI5ViaCep.controller.js
+++ controller/AppUI5ViaCep.controller.js
@@ -1,6 +1,7 @@
+
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-	"sap/ui/model/Context"
+    "sap/ui/model/Context"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -9,79 +10,92 @@ sap.ui.define([
         "use strict";
         
         return Controller.extend("treinaui5.sapui5viacep.controller.AppUI5ViaCep", {
-
-            onInit: function() {
-                
-            },
-            onAfterRendering(){
-                var panel1 = $("#application-treinaui5sapui5viacep-display-component---AppUI5ViaCep--panel1");
-                var panel2 = $("#application-treinaui5sapui5viacep-display-component---AppUI5ViaCep--panel2");
-                var panel3 = $("#application-treinaui5sapui5viacep-display-component---AppUI5ViaCep--panel3");
-                var btn1   = $("#application-treinaui5sapui5viacep-display-component---AppUI5ViaCep--_IDGenButton1");
-                panel2.hide();
-                panel3.hide();
-                btn1.hide();
-            },
-            pesquisarCep: function (event, _z){
-                var panel1 = $("#application-treinaui5sapui5viacep-display-component---AppUI5ViaCep--panel1");
-                var panel2 = $("#application-treinaui5sapui5viacep-display-component---AppUI5ViaCep--panel2");
-                var panel3 = $("#application-treinaui5sapui5viacep-display-component---AppUI5ViaCep--panel3");
-                var btn1   = $("#application-treinaui5sapui5viacep-display-component---AppUI5ViaCep--_IDGenButton1");
-
-                var getCep = event.getSource().getValue();
-                var urlReq = "https://viacep.com.br/ws/" + getCep + "/json";
-                
-                $.ajax({
-                    type: "GET",
-                    crossDomain: true,
-                    url: urlReq,
-                    success: function (res, status, xhr) {
-                        console.log(res);
-                        console.log(status);
-                        console.log(xhr);
-
-                        var erro = res.erro;
-                        console.log(erro);
-                        if (erro == true){
-                            alert("O CEP " + getCep + " não existe!" );
-                            panel1.show();
-                            panel2.hide();
-                            panel3.hide();
-                            btn1.hide();
-                            return;
-                        }
-
-                        $("input[name='logradouro']").val(res.logradouro);
-                        $("input[name='bairro']").val(res.bairro);
-                        $("input[name='estado']").val(res.uf);
-                        $("input[name='cidade']").val(res.localidade);
-                        $("input[name='cep']").val(res.cep);
-                        panel1.hide();
-                        panel2.show();
-                        panel3.show();
-                        btn1.show();
-                        
-                    },
-                    error: function(data){
-                        console.log(data);
-                    }
-                });       
-            },
             
-		    limparEndereco: function (_oEvent, _y, _z){
-                var panel1 = $("#application-treinaui5sapui5viacep-display-component---AppUI5ViaCep--panel1");
-                var panel2 = $("#application-treinaui5sapui5viacep-display-component---AppUI5ViaCep--panel2");
-                var panel3 = $("#application-treinaui5sapui5viacep-display-component---AppUI5ViaCep--panel3");
-                var btn1   = $("#application-treinaui5sapui5viacep-display-component---AppUI5ViaCep--_IDGenButton1");
-                $("input[name='logradouro']").val("");
-                $("input[name='bairro']").val("");
-                $("input[name='estado']").val("");
-                $("input[name='cidade']").val("");
-                $("input[name='cep']").val("");
-                panel1.show();
-                panel2.hide();
-                panel3.hide();
-                btn1.hide();
+            onInit: function () {
+
+            },
+            onAfterRendering() {
+                var panel2 = this.byId("panel2");
+                var panel3 = this.byId("panel3");
+                var btn1   = this.byId("_IDGenButton1");
+                panel2.setVisible(false);
+                panel3.setVisible(false);
+                btn1.setVisible(false);
+            },
+            pesquisarCep: function (event, _z) {
+                var panel1     = this.byId("panel1");
+                var panel2     = this.byId("panel2");
+                var panel3     = this.byId("panel3");
+                var btn1       = this.byId("_IDGenButton1");
+                var cidade     = this.byId("cidade");
+                var logradouro = this.byId("logradouro");
+                var bairro     = this.byId("bairro");
+                var cep        = this.byId("cep");
+                var estado     = this.byId("estado");
+                var getCep = event.getSource().getValue();
+
+                if (getCep !== '') {
+                    var urlReq = "https://viacep.com.br/ws/" + getCep + "/json";
+                    $.ajax({
+                        type: "GET",
+                        crossDomain: true,
+                        url: urlReq,
+                        success: function (res, status, xhr) {
+                            var erro = res.erro;
+                            console.log(erro);
+                            if (erro == true) {
+                                panel2.setVisible(false);
+                                panel3.setVisible(false);
+                                btn1.setVisible(false);
+                                alert("O CEP " + getCep + " não existe!");
+                                return;
+                            }
+                            console.log(res);
+                            
+                            panel1.setVisible(false);
+                            panel2.setVisible(true);
+                            panel3.setVisible(true);
+                            btn1.setVisible(true);
+
+                            cidade.setValue(res.localidade);
+                            cep.setValue(res.cep);
+                            logradouro.setValue(res.logradouro);
+                            estado.setValue(res.uf);
+                            bairro.setValue(res.cep);
+                            
+                        },
+                        error: function (data) {
+                            console.log(data);
+                        }
+                    });
+                }
+                else{
+                    alert("Precisa digitar o CEP");
+                    return;
+                }
+            },
+
+            limparEndereco: function (_oEvent, _y, _z) {
+                var panel1 = this.byId("panel1");
+                var panel2 = this.byId("panel2");
+                var panel3 = this.byId("panel3");
+                var btn1 = this.byId("_IDGenButton1");
+                var cidade     = this.byId("cidade");
+                var logradouro = this.byId("logradouro");
+                var bairro     = this.byId("bairro");
+                var cep        = this.byId("cep");
+                var estado     = this.byId("estado");
+
+                cidade.setValue("");
+                cep.setValue("");
+                logradouro.setValue("");
+                estado.setValue("");
+                bairro.setValue("");
+
+                panel1.setVisible(true);
+                panel2.setVisible(false);
+                panel3.setVisible(false);
+                btn1.setVisible(false);
             },
 
         });
